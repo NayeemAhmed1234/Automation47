ccc5ecb28a9352da1380d19c9a355fe8969e2a9b
Admin <Admin@DESKTOP-5AB338K>
2023-04-04 11:05:33 +0530

    two classes added

---
+++ src/test/java/codes/ClassA.java
@@ -0,0 +1,8 @@
+package codes;
+
+public class ClassA {
+	public void hello() {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/codes/ClassB.java
@@ -0,0 +1,7 @@
+package codes;
+
+public class ClassB {
+public void hiii() {
+	System.out.println("hii");
+}
+}
